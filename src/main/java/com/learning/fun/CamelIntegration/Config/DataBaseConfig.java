7b9e1eeefe5988f5f87079e6e91fa85483877229
vikas2453 <vikas2453@gmail.com>
2019-09-15 17:36:54 -0400

    Initial setup

--- src/main/java/com/learning/fun/CamelIntegration/Config/DataBaseConfig.java
+++ src/main/java/com/learning/fun/CamelIntegration/Config/DataBaseConfig.java
@@ -2,6 +2,7 @@ package com.learning.fun.CamelIntegration.Config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,15 +10,35 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataBaseConfig {
-	
+
+	/*
+	 * Below method and property are only required if you want to set a different
+	 * data pool other Hik
+	 * 
+	 * @Value("${spring.datasource.url}") private String JdbcUrl;
+	 * 
+	 * @Value("${spring.datasource.driverClassName}") private String
+	 * driverClassName;
+	 * 
+	 * @Value("${spring.datasource.username}") private String username;
+	 * 
+	 * @Value("${spring.datasource.password}") private String password;
+	 * 
+	 * @Bean
+	 * 
+	 * @ConfigurationProperties(prefix="spring.datasource") public DataSource
+	 * createDataSource() { BasicDataSource ds = new BasicDataSource();
+	 * ds.setUrl(JdbcUrl); ds.setUsername(username); ds.setPassword(password);
+	 * ds.setDriverClassName(driverClassName); return ds;
+	 * 
+	 * }
+	 */
+
 	@Bean
-	@ConfigurationProperties(prefix="spring.datasource")
+	@ConfigurationProperties(prefix = "spring.datasource")
 	public DataSource createDataSource() {
-		DataSource dataSource =DataSourceBuilder.create().build();	
-		
-		return dataSource;	
-		
+		DataSource dataSource = DataSourceBuilder.create().build();
+		return dataSource;
 	}
-	
 
 }
---
+++ src/main/java/com/learning/fun/CamelIntegration/Config/FileRoute.java
@@ -0,0 +1,14 @@
+package com.learning.fun.CamelIntegration.Config;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		//from("file:C:\\Users\\vikas\\OneDrive\\Pictures\\iCloud Photos\\Downloads?noop=true").to("file:E:\\old computer photos\\PHOTOS\\US\\2019\\temp\\");
+	}
+}
+
