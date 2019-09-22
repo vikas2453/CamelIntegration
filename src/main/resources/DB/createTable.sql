1535886d1f4dba7675801f889bcaa7273e5d9110
vikas2453 <vikas2453@gmail.com>
2019-09-22 13:38:06 -0400

    Sql related to create table

---
+++ src/main/resources/DB/createTable.sql
@@ -0,0 +1,1 @@
+create table CamelProcessing(id integer not null primary key, processorName varchar(20) not null, payload varchar(300), status varchar(20));
---
+++ src/main/resources/DB/insertEntires.sql
@@ -0,0 +1,3 @@
+insert into camelprocessing values(1, 'OrderProcessor', '{"firstName":"Vikas", "email":"vikas2453@gmail.ccom"}', 'N');
+
+insert into camelprocessing values(2, 'OrderProcessor', '{"firstName":"ikas", "email":"vikas243@gmail.ccom"}', 'N');
